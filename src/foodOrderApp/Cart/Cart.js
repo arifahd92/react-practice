cfb9e25339867813905c9586b177711b754959de
md arif <arifahd92@gmail.com>
2023-06-10 08:50:39 +0530

    model

--- src/foodOrderApp/Cart/Cart.js
+++ src/foodOrderApp/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -10,17 +11,19 @@ const Cart = (props) => {
     );
 
     return (
-        <div>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
+                <button className={classes['button--alt']} onClick={props.onClose}>
+                    Close
+                </button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     );
 };
 
